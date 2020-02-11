4e53dfb5e59683a6d9c5901bf718d0e71359e608
Sunshine-creator <941389138@163.com>
2020-02-11 17:58:21 +0800

    练习代码

---
+++ Test.java
@@ -0,0 +1,45 @@
+import java.nio.channels.InterruptibleChannel;
+
+public class Test {
+    public static void main(String[] args) {
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        int maxVaule = Integer.MAX_VALUE;
+//        System.out.println(maxVaule+1);
+//        int minVaule = Integer.MIN_VALUE;
+//        System.out.println(minVaule);
+//        char ch = '呵';
+//        System.out.println(ch);
+//          String name ="zhaoyun";
+//          System.out.println(name);
+       /* String  a = "hello";
+        String  b =  "world";
+        String c =  a +b;
+        System.out.println(c);
+        String str = "result=";
+        int e =10;
+        int d =20;
+        String result = str +e + d;
+        System.out.println(result);
+*/
+//
+//        int num =10;
+////        String str=  String.valueOf(num);
+////        int a =  30;
+////        String str1 = num +"";
+////        String str2 = "100";
+////        int num1 =  Integer.parseInt(str2 );
+//           int a= 10;
+//           int b =20;
+//        System.out.println(a&b);
+//        System.out.println(a|b);
+//         int a =  0x1;
+//         int b  = 0x2;
+//         System.out.println(a^b);
+//        int a =0x10;
+//        System.out.println(a<<1);
+//        System.out.println(a>>1);
+
+
+    }
+}
